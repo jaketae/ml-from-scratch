4653925a67ba6940c61055283d212e33996b1f82
“jaketae” <jaesungtae@gmail.com>
2020-03-18 20:36:19 +0900

    Rearrange directory structure

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,45 +1,37 @@
+import math
 import numpy as np
-
-def cross_entropy(y_true, y_pred):
-    n_samples = len(y_true)
-    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
-    total_entropy = - np.sum(
-        np.multiply(y_true, np.log(y_pred)) + 
-        np.multiply((1 - y_true), np.log(1 - y_pred)))
-    return total_entropy / n_samples
+from utils.activation_functions import Sigmoid
 
 class LogisticRegression:
 
-    def __init__(self, learning_rate=0.001, n_iters=1000):
-        self.alpha = learning_rate
+    def __init__(self, learning_rate, n_iters):
+        self.learning_rate = learning_rate
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
+        self.sigmoid = Sigmoid()
 
-    def fit(self, X, y, verbose=False):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
+    def _init_weights(self, X):
+        self.n_samples, self.n_features = X.shape
+        limit = 1 / math.sqrt(self.n_features)
+        self.weights = np.random.uniform(-limit, limit, (self.n_features,))
+        self.bias = 0
 
-        for i in range(self.n_iters):
-            z = np.dot(X, self.weights) + self.bias
-            y_predicted = self._sigmoid(z)
+    def fit(self, X, y):
+        self._init_weights(X)
 
-            if verbose:
-                cost = cross_entropy(y, y_pred)
-                print("Iteration {0}, Cost: {1}".format(i, cost))
-                
-            dw = np.dot(X.T, (y_predicted - y)) / n_samples
-            db = np.sum(y_predicted - y) / n_samples
+        for _ in range(self.n_iters):
+            y_predicted = self.sigmoid(np.dot(X, self.weights) + self.bias)
+            
+            dw = np.dot(X.T, (y_predicted - y)) / self.n_samples
+            db = np.sum(y_predicted - y) / self.n_samples
 
-            self.weights -= self.alpha * dw
-            self.bias -= self.alpha * db
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
 
     def predict(self, X):
-        z = np.dot(X, self.weights) + self.bias
-        y_predicted = self._sigmoid(z)
+        y_predicted = self.sigmoid(np.dot(X, self.weights) + self.bias)
         vector_int = np.vectorize(int)
         y_predicted_cls = vector_int(y_predicted > 0.5)
         return y_predicted_cls
 
-    def _sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
---
+++ tests/logistic_regression_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from models.logistic_regression import LogisticRegression
+
+X, y = datasets.load_breast_cancer(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+regressor = LogisticRegression(learning_rate=0.001, n_iters=1000)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+print("Accuracy: {}".format(accuracy_score(y_test, y_pred)))
